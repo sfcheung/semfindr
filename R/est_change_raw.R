dd4345e3c7c0ce9b647eda462dc9ff6bf1bb4b0a
Shu Fai Cheung <shufai.cheung@gmail.com>
2020-06-17 00:23:16 +0800

    Add est_change_raw
    This can compute the raw paramter change

---
+++ R/est_change_raw.R
@@ -0,0 +1,108 @@
+#'@title
+#' Case influence on parameter estimates
+#'
+#'@description
+#' Get a [lavaan_rerun()] output and compute the changes in selected parameters
+#' for each case. 
+#'
+#'@details
+#' For each case, compute the differences in the estimates of selected parameter
+#' between rerun without
+#' this case and the original fit with this case.
+#' The change is the raw change, either for the standardized or
+#' unstandardized solution. The change is *not* divided by standard error.#'
+#' Currently only work for one group analysis.
+#'
+#'@param rerun_out The output from [lavaan_rerun()].
+#'@param parameters A vector of characters to specify the selected parameters.
+#'                  Each element is of this form: `x ~ y` or `x ~~ y`, 
+#'                  corresponds to how each parameter is specified in `lavaan`.
+#'                  The naming convention of a `lavaan` output can be found
+#'                  by [lavaan::parameterEstimates()]. If `NULL`, the default,
+#'                  differences on all parameters will be computed.
+#'@param standardized If `TRUE`, the changes in full standardized is returned.
+#'                    Otherwise, the changes in unstandardized solution is returned.
+#'                    Default is `FALSE`.
+#'
+#'@return
+#'A matrix with the number of columns equal to the number of requested parameters,
+#'and the number of rows equal to the number of cases. The row names is the 
+#'case identification values used in [lavaan_rerun()]. The elements are the 
+#'standardized difference. Please see Pek and MacCallum (2011), Equation 7.
+#'
+#'@examples
+#'# To be prepared.
+#'
+#'@references
+#'Pek, J., & MacCallum, R. (2011). Sensitivity analysis in structural equation models: Cases and their influence. *Multivariate Behavioral Research, 46*(2), 202–228. <https://doi.org/10.1080/00273171.2011.561068>
+#'
+#'@export
+#'@importMethodsFrom lavaan vcov
+
+est_change_raw <- function(rerun_out,
+                       parameters = NULL,
+                       standardized = FALSE
+                       )
+{
+  if (missing(rerun_out)) {
+      stop("No lavaan_rerun output supplied.")
+    }
+  case_ids <- names(rerun_out$rerun)
+  reruns <- rerun_out$rerun
+  fit0   <- rerun_out$fit
+  est0   <- lavaan::parameterEstimates(
+              fit0,
+              se = FALSE,
+              zstat = FALSE,
+              pvalue = FALSE,
+              ci = FALSE,
+              standardized = TRUE,
+              fmi = FALSE,
+              cov.std = TRUE,
+              rsquare = FALSE,
+              remove.nonfree = TRUE,
+              output = "data.frame"
+              )
+  parameters_names <- paste0(est0$lhs, est0$op, est0$rhs)
+  if (!is.null(parameters)) {
+    parameters_selected <- gsub(" ", "", parameters)
+    if (!all(parameters_selected %in% parameters_names)) {
+        stop("Not all parameters can be found in the output. Please check the parameters argument.")
+      }
+    } else {
+      parameters_selected <- parameters_names
+    }
+  tmpfct <- function(x, est, parameters_names,
+                             parameters_selected) {
+    esti_full <- lavaan::parameterEstimates(
+                  x,
+                  se = TRUE,
+                  zstat = FALSE,
+                  pvalue = FALSE,
+                  ci = FALSE,
+                  standardized = TRUE,
+                  fmi = FALSE,
+                  cov.std = TRUE,
+                  rsquare = FALSE,
+                  remove.nonfree = TRUE,
+                  output = "data.frame"
+                  )
+    if (standardized) {
+        esti_change <- (est$std.all - esti_full$std.all)
+      } else {
+        esti_change <- (est$est - esti_full$est)
+      }
+    names(esti_change) <- parameters_names
+    outi <- esti_change[parameters_selected]
+    names(outi) <- parameters_selected
+    outi
+  }
+  out <- sapply(reruns,
+                tmpfct,
+                est = est0,
+                parameters_names = parameters_names,
+                parameters_selected = parameters_selected
+              )
+  out <- t(out)
+  out
+}
