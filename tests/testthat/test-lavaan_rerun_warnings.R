80cc671a6e9c6d8d01902bcfda27ead42a4a2edf
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-08-26 09:58:15 +0800

    Use pregenerated dataset to avoid the platform dependent error
    Tests and checks passed.

--- tests/testthat/test-lavaan_rerun_warnings.R
+++ tests/testthat/test-lavaan_rerun_warnings.R
@@ -4,21 +4,19 @@ library(semfindr)
 
 #context("Test handling of reruns with warnings")
 
-mod_p <- 
+mod_p <-
 '
 f1 =~  .7*x1 + .6*x2 + .8*x3 + .3*x5
 f2 =~  .2*x1 + .6*x4 + .8*x5 + .7*x6
 f1 ~~ .2*f2
 '
-mod <- 
+mod <-
 '
 f1 =~ x1 + x2 + x3
 f2 =~ x4 + x5 + x6
 '
 
-# generate data
-set.seed(48157438)
-dat <- lavaan::simulateData(mod_p, sample.nobs =  45L)
+dat <- cfa_dat[1:45, ]
 fit <- lavaan::cfa(mod, dat)
 #summary(fit, fit.measures = TRUE)
 fit_rerun <- lavaan_rerun(fit)
@@ -27,15 +25,15 @@ fit_post_check <- sapply(fit_rerun$rerun, function(x) {
                                     warning = function(w) w)
                     })
 i_valid <- sapply(fit_post_check, isTRUE)
-fit_est_change1 <- est_change(fit_rerun)                    
-fit_est_change_raw1 <- est_change_raw(fit_rerun)                    
-fit_est_change2 <- est_change(fit_rerun, c("f1 =~ x2", "f2 =~ x5"))                    
-fit_est_change_raw2 <- est_change_raw(fit_rerun, c("f1 =~ x2", "f2 =~ x5"))                    
-fit_est_change3 <- est_change(fit_rerun, c("f2 =~ x5"))                    
-fit_est_change_raw3 <- est_change_raw(fit_rerun, c("f2 =~ x5"))                    
-fit_fm_change1 <- fit_measures_change(fit_rerun, c("chisq", "cfi"))                    
-fit_fm_change2 <- fit_measures_change(fit_rerun, c("tli"))                    
-                    
+fit_est_change1 <- est_change(fit_rerun)
+fit_est_change_raw1 <- est_change_raw(fit_rerun)
+fit_est_change2 <- est_change(fit_rerun, c("f1 =~ x2", "f2 =~ x5"))
+fit_est_change_raw2 <- est_change_raw(fit_rerun, c("f1 =~ x2", "f2 =~ x5"))
+fit_est_change3 <- est_change(fit_rerun, c("f2 =~ x5"))
+fit_est_change_raw3 <- est_change_raw(fit_rerun, c("f2 =~ x5"))
+fit_fm_change1 <- fit_measures_change(fit_rerun, c("chisq", "cfi"))
+fit_fm_change2 <- fit_measures_change(fit_rerun, c("tli"))
+
 test_that("Check if lavTech post.check results are stored.", {
     expect_equal(ignore_attr = TRUE,
         fit_rerun$post_check, fit_post_check
