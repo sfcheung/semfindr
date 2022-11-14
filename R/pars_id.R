5b0539827c54a8b47c3f6de09b57423c96453b06
Shu Fai Cheung <shufai.cheung@gmail.com>
2022-11-14 21:28:51 +0800

    Add pars_id and its tests
    Tests and checks passed.

---
+++ R/pars_id.R
@@ -0,0 +1,56 @@
+#' @title Convert Parameter Syntax to Ids in the Parameter
+#' Vectors
+#'
+#' @description An internal function to convert a vector of
+#' parameters specified in lavaan syntax to the ids of them
+#' in the vector of free parameters.
+#'
+#' @details It uses [lavaan::lavaanify()] to parse the
+#' syntax strings. Multiple sample models is supported.
+#'
+#' @return
+#' A numeric vector of the ids in the column "free" in the
+#' parameter table of the fit object.
+#'
+#' @param pars A character vector of parameters specified
+#' in lavaan syntax, e.g., `"y ~ x"` and `f1 =~ x3`. For
+#' multisample models, if only the parameters in some groups
+#' are needed, use the modifier for labeling parameters and
+#' use `NA` to denote parameters to be requested. E.g.,
+#' `f1 =~ c(NA, 0, NA, NA) * x2` denotes the loadings of
+#' `x2` on `f1` in the first, third, and fourth groups.
+#'
+#' @param fit A `lavaan`-class object. This object is used
+#' to determine the number of groups and the parameters
+#' in the model. Only parameters in `pars` that appear in
+#' this model will be considered.
+#'
+#' @author Shu Fai Cheung <https://orcid.org/0000-0002-9871-9448>
+#'
+#' @examples
+#' \donttest{
+#' }
+#'
+#' @noRd
+
+pars_id <- function(pars,
+                    fit) {
+    pfree <- lavaan::lavInspect(fit, "npar")
+    ngp <- lavaan::lavInspect(fit, "ngroups")
+    ptable <- lavaan::parameterTable(fit)
+    parspt <- tryCatch(lavaan::lavaanify(pars, ngroups = ngp),
+                       error = function(e) e)
+    if (inherits(parspt, "simpleError")) {
+        stop(paste0("Error in parameter syntax. This is the lavaan error message:",
+                    "\n",
+                    parspt$message))
+      }
+    parspt2 <- as.data.frame(lavaan::lavParseModelString(pars))
+    mcol <- c("lhs", "op", "rhs", "group", "free")
+    parspt3 <- merge(parspt[, mcol],
+                     parspt2)[, mcol]
+    parspt3 <- parspt3[parspt3$free > 0, ]
+    parspt4 <- merge(parspt3[, -which(mcol == "free")], ptable)
+    out <- parspt4$free
+    out
+  }
--- tests/testthat/test-parameter_id.R
+++ tests/testthat/test-parameter_id.R
@@ -1,8 +1,5 @@
-skip("WIP")
 library(testthat)
 library(lavaan)
-
-#context("Test mahalanobis_rerun.R")
 
 dat <- sem_dat
 set.seed(64264)
@@ -48,34 +45,27 @@ fit_gp_eq@Model@eq.constraints
 pars1 <- c("f1 =~ x2", "f2 =~ x5", "f2 ~ f1")
 pars2 <- c("f1 =~ x2", "f2 =~ c(NA, 1, NA) * x5", "f2 ~ f1")
 pars3 <- c("f1 =~ x2", "f2 =~ c(NA, 1, NA) * x5", "f2 ~ c(1, NA, 1) * f1")
-pars_id <- function(pars,
-                   fit) {
-    pfree <- lavaan::lavInspect(fit, "npar")
-    ngp <- lavaan::lavInspect(fit, "ngroups")
-    ptable <- lavaan::parameterTable(fit)
-    parspt <- tryCatch(lavaan::lavaanify(pars, ngroups = ngp),
-                       error = function(e) e)
-    if (inherits(parspt, "simpleError")) {
-        stop(paste0("Error in parameter syntax. This is the lavaan error message:",
-                    "\n",
-                    parspt$message))
-      }
-    parspt2 <- as.data.frame(lavaan::lavParseModelString(pars))
-    mcol <- c("lhs", "op", "rhs", "group", "free")
-    parspt3 <- merge(parspt[, mcol],
-                     parspt2)[, mcol]
-    parspt3 <- parspt3[parspt3$free > 0, ]
-    parspt4 <- merge(parspt3[, -which(mcol == "free")], ptable)
-    out <- parspt4$free
-    out
-  }
 
-(id11 <- pars_id(pars1, fit_ng))
-(id12 <- pars_id(pars2, fit_ng))
-(id13 <- pars_id(pars3, fit_ng))
-(id21 <- pars_id(pars1, fit_gp))
-(id22 <- pars_id(pars2, fit_gp))
-(id23 <- pars_id(pars3, fit_gp))
+# Test conditions
+# Single-group model / Single-group specification
+# Single-group model / Multi-group specification
+# Single-group model with equality constraints / Single-group specification
+# Single-group model with equality constraints / Multi-group specification
+# Multi-group model with equality constraints / Single-group specification
+# Multi-group model with equality constraints / Multi-group specification
+
+pt_ng <- parameterTable(fit_ng)
+pt_ng_eq <- parameterTable(fit_ng_eq)
 pt_gp <- parameterTable(fit_gp)
-tmp <- pt_gp[pt_gp$free %in% id23, ]
-tmp[order(tmp$lhs, tmp$op, tmp$rhs, tmp$group), ]
+
+test_that("pars_id", {
+    expect_true(all(pars_id(pars1, fit_ng) %in% c(1, 3, 7)))
+    expect_error(pars_id(pars2, fit_ng))
+    expect_error(pars_id(pars3, fit_ng))
+    expect_true(all(pars_id(pars1, fit_ng_eq) %in% c(1, 3, 7)))
+    expect_error(pars_id(pars2, fit_ng_eq))
+    expect_error(pars_id(pars3, fit_ng_eq))
+    expect_true(all(pars_id(pars1, fit_gp) %in% c(1, 3, 7, 30, 32, 36, 59, 61, 65)))
+    expect_true(all(pars_id(pars2, fit_gp) %in% c(1, 3, 7, 30, 36, 59, 61, 65)))
+    expect_true(all(pars_id(pars3, fit_gp) %in% c(1, 3, 30, 36, 59, 61)))
+  })
