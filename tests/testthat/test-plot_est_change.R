4b38e9344278395ccebbd39d84891a025bc58bab
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-02-09 08:05:20 +0800

    WIP. Have bugs.

--- tests/testthat/test-plot_est_change.R
+++ tests/testthat/test-plot_est_change.R
@@ -29,6 +29,11 @@ fit_est_change_raw <- est_change_raw(rerun_out)
 fit_est_change_raw_approx <- est_change_raw_approx(fit)
 fit_est_change_raw_std <- est_change_raw(rerun_out,
                                          standardized = TRUE)
+set.seed(89235)
+n <- nrow(dat0)
+random_case_ids <- paste0("case_", sample.int(n))
+fit_est_change_approx_id <- est_change_approx(fit,
+                                              case_id = random_case_ids)
 
 gcd_method <- function(x,
                        gcd_names = c("gcd", "gcd_approx")) {
@@ -55,6 +60,7 @@ est_to_long <- function(x) {
 
 tmp1 <- est_to_long(fit_est_change)
 tmp2 <- est_to_long(fit_est_change_approx)
+tmp3 <- est_to_long(fit_est_change_approx_id)
 
 test_that("Test est_to_long", {
     expect_true("gcd" %in% colnames(tmp1))
@@ -66,7 +72,9 @@ test_that("Test est_to_long", {
   })
 
 plot_est_change <- function(x,
-                            params) {
+                            params,
+                            cutoff_gcd = NULL,
+                            largest_gcd = 1) {
     gcd_name <- gcd_method(x)
     approx <- ifelse(gcd_name == "gcd_approx", TRUE, FALSE)
     x0 <- est_to_long(x)
@@ -78,6 +86,31 @@ plot_est_change <- function(x,
                              y = .data[["change"]])) +
           ggplot2::geom_point() +
           ggplot2::geom_hline(yintercept = 0)
+    if (is.numeric(cutoff_gcd)) {
+        p <- p + ggplot2::geom_vline(xintercept = cutoff_gcd,
+                                     linetype = "dashed")
+        c_gcd_cut <- cutoff_gcd
+      } else {
+        c_gcd_cut <- Inf
+      }
+    if (is.numeric(largest_gcd) && largest_gcd >= 1) {
+        gcd_tmp <- x0[x0$param == x0$param[1], c("case",
+                                                 gcd_name)]
+        m_gcd <- round(largest_gcd)
+        o_gcd <- order(gcd_tmp[[gcd_name]],
+                               decreasing = TRUE)
+        m_gcd_cut <- gcd_tmp[[gcd_name]][o_gcd[m_gcd]]
+      } else {
+        m_gcd_cut <- Inf
+      }
+    label_gcd <- (x0[[gcd_name]] >= c_gcd_cut) |
+                 (x0[[gcd_name]] >= m_gcd_cut)
+    p <- p + ggrepel::geom_label_repel(
+                data = x0[label_gcd, ],
+                ggplot2::aes(x = .data[[gcd_name]],
+                             y = .data[["change"]],
+                             label = .data[["case"]]),
+                position = ggplot2::position_dodge(.5))
     p <- p + ggplot2::facet_wrap(ggplot2::vars(.data[["param"]]),
                         ncol = 1,
                         scales = "free_x",
@@ -90,10 +123,14 @@ plot_est_change <- function(x,
 
 params <- c("m1~iv1", "a2", "b")
 plot_est_change(fit_est_change, params = params)
+plot_est_change(fit_est_change, params = params, largest_gcd = 3)
+plot_est_change(fit_est_change, params = params, cutoff_gcd = .2)
 params <- c("m1~iv1", "a2", "b")
 plot_est_change(fit_est_change_raw, params = params)
 params <- c("m1~iv1", "m1~iv2", "dv~m1")
 plot_est_change(fit_est_change_approx, params = params)
+plot_est_change(fit_est_change_approx, params = params, cutoff_gcd = .1)
+plot_est_change(fit_est_change_approx, params = params, cutoff_gcd = .2)
 
 # CFA model with selected loadings
 
