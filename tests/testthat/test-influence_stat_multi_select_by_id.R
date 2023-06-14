c0c3e6c7e22d84d4fc9618784f559addbcf47836
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-06-14 16:35:23 +0800

    More tests for influence_stat for multiple-group models

--- tests/testthat/test-influence_stat_multi_select_by_id.R
+++ tests/testthat/test-influence_stat_multi_select_by_id.R
@@ -19,8 +19,8 @@ dat0$gp <- sample(c("gp2", "gp1"), size = nrow(dat0), replace = TRUE)
 
 fit0 <- lavaan::sem(mod, dat0, group = "gp")
 
-rerun_out <- lavaan_rerun(fit0, parallel = FALSE,
-                          to_rerun = c(1:10))
+rerun_out <- lavaan_rerun(fit0, to_rerun = c(1, 3, 9, 15, 50), parallel = FALSE)
+rerun_15 <- rerun_out$rerun[[4]]
 
 md_rerun   <- mahalanobis_rerun(rerun_out)
 fm_rerun   <- fit_measures_change(rerun_out)
