b388a589dcc47e24cc470c82b32c758188a51aff
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-10-05 20:10:10 +0800

    Add index_plot

--- R/generic_index_plot.R
+++ R/generic_index_plot.R
@@ -1,8 +1,25 @@
-#' @title TBD
+#' @title Index Plot of an Arbitrary Statistic
 #'
-#' @description TBD
+#' @description A generic index plot
+#' function for plotting values of
+#' a column # in a matrix.
 #'
-#' @details TBD
+#' @details This index plot function
+#' is for plotting any measure of
+#' influence or extremeness in a matrix.
+#' It can be used for measures not supported
+#' with other functions.
+#'
+#' Like functions such as [gcd_plot()]
+#' and [est_change_plot()], it supports
+#' labelling cases based on the values
+#' on the selected measure
+#' (originaL values or absolute values).
+#'
+#' Users can also plot cases based on
+#' the absolute values. This is useful
+#' when cases are to be compared on
+#' magnitude, ignoring the sign.
 #'
 #' @param object A matrix-like object,
 #' such as the output from
@@ -69,6 +86,14 @@
 #' `list()` and internal default
 #' settings will be used.
 #'
+#' @param hline_aes A named list of
+#' arguments to be passed to
+#' [ggplot2::geom_hline()] to modify how
+#' to draw the horizontal line for zero
+#' case influence. Default is `list()`
+#' and internal default settings will be
+#' used.
+#'
 #' @param cutoff_line_aes A named list
 #' of arguments to be passed to
 #' [ggplot2::geom_hline()] to modify how
@@ -107,8 +132,22 @@
 #' "
 #' # Fit the model
 #' fit <- lavaan::sem(mod, dat)
+#' summary(fit)
 #'
-#' # TBD
+#' # --- Leave-One-Out Approach
+#'
+#' # Fit the model n times. Each time with one case removed.
+#' # For illustration, do this only for selected cases.
+#' fit_rerun <- lavaan_rerun(fit, parallel = FALSE,
+#'                           to_rerun = 1:10)
+#' # Get all default influence stats
+#' out <- influence_stat(fit_rerun)
+#'
+#' # Plot case influence on chi-square. Label the 3 cases with the influence.
+#' index_plot(out, "chisq", largest_x = 3)
+#'
+#' # Plot absolute case influence on chi-square.
+#' index_plot(out, "chisq", absolute = TRUE)
 #'
 #' @seealso [influence_stat()], [est_change()],
 #' [est_change_raw()]
@@ -126,6 +165,7 @@ index_plot <- function(object,
                        absolute = FALSE,
                        point_aes = list(),
                        vline_aes = list(),
+                       hline_aes = list(),
                        cutoff_line_aes = list(),
                        case_label_aes = list()
                        ) {
@@ -169,6 +209,12 @@ index_plot <- function(object,
                                   list(mapping = ggplot2::aes(
                                             xend = .data[["row_id"]],
                                             yend = 0)))
+  hline_aes <- utils::modifyList(list(linetype = "solid",
+                                      color = "grey"),
+                                  hline_aes)
+  # The following part should never be changed by users.
+  hline_aes <- utils::modifyList(hline_aes,
+                                  list(yintercept = 0))
 
   case_ids <- names(object)
   row_id   <- seq_len(length(object))
@@ -181,6 +227,7 @@ index_plot <- function(object,
   p <- p + do.call(ggplot2::geom_point, point_aes)
   p <- p + ggplot2::labs(title = plot_title)
   p <- p + do.call(ggplot2::geom_segment, vline_aes)
+  p <- p + do.call(ggplot2::geom_hline, hline_aes)
   p <- p + ggplot2::xlab("Case ID (or Row Number)") +
          ggplot2::ylab(x_label)
 
@@ -227,362 +274,6 @@ index_plot <- function(object,
                              mapping = ggplot2::aes(
                                 x = .data[["row_id"]],
                                 y = .data[["x"]],
-                                label = .data[["case_id"]])))
-  p <- p + do.call(ggrepel::geom_label_repel, case_label_aes)
-  p
-}
-
-#' @importFrom rlang .data
-#' @noRd
-
-md_plot_tmp <- function(influence_out,
-                    cutoff_md = FALSE,
-                    cutoff_md_qchisq = .975,
-                    largest_md = 1,
-                    point_aes = list(),
-                    vline_aes = list(),
-                    cutoff_line_aes = list(),
-                    case_label_aes = list()
-                    ) {
-  if (missing(influence_out)) {
-      stop("No influence_stat output supplied.")
-    }
-  if (!inherits(attr(influence_out, "fit"), "lavaan")) {
-      stop(paste("The original lavaan output is not in the attributes.",
-                 "Was subsetting used to get influence_out?"))
-    }
-
-  point_aes <- utils::modifyList(list(),
-                                  point_aes)
-
-  vline_aes <- utils::modifyList(list(linewidth = 1,
-                                      lineend = "butt"),
-                                  vline_aes)
-  # The following part should never be changed by users.
-  vline_aes <- utils::modifyList(vline_aes,
-                                  list(mapping = ggplot2::aes(
-                                            xend = .data[["row_id"]],
-                                            yend = 0)))
-
-  fit0 <- attr(influence_out, "fit")
-  case_ids <- rownames(influence_out)
-  row_id   <- seq_len(nrow(influence_out))
-  dat <- data.frame(row_id = row_id,
-                    case_id = case_ids,
-                    influence_out,
-                    stringsAsFactors = FALSE,
-                    check.names = FALSE)
-  if (all(is.na(dat$md))) {
-      stop("All cases have no value on Mahalanobis distance (md).")
-    }
-
-  p <- ggplot2::ggplot(dat, ggplot2::aes(.data$row_id, .data$md))
-  p <- p + do.call(ggplot2::geom_point, point_aes)
-  p <- p + do.call(ggplot2::geom_segment, vline_aes)
-  p <- p + ggplot2::labs(title = "Mahalanobis Distance") +
-         ggplot2::xlab("Row Number") +
-         ggplot2::ylab("Mahalanobis Distance")
-
-  k <- ncol(fit0@Data@X[[1]])
-  c_md_cut <- Inf
-  if (isTRUE(cutoff_md)) {
-      c_md_cut <- stats::qchisq(cutoff_md_qchisq, k)
-    }
-  if (is.numeric(cutoff_md)) {
-      c_md_cut <- cutoff_md
-    }
-  if (is.numeric(largest_md) && largest_md >= 1) {
-      m_md <- round(largest_md)
-      o_md <- order(dat$md, decreasing = TRUE)
-      m_md_cut <- dat$md[o_md[m_md]]
-    } else {
-      m_md_cut <- Inf
-    }
-  if (is.numeric(c_md_cut) && c_md_cut < Inf) {
-      cutoff_line_aes <- utils::modifyList(list(linetype = "dashed"),
-                                                cutoff_line_aes)
-      # The following part should never be changed by users.
-      cutoff_line_aes <- utils::modifyList(cutoff_line_aes,
-                                    list(yintercept = c_md_cut))
-      p <- p + do.call(ggplot2::geom_hline, cutoff_line_aes)
-    }
-  label_md <- (dat$md >= c_md_cut) | (dat$md >= m_md_cut)
-
-  case_label_aes <- utils::modifyList(list(position = ggplot2::position_dodge(.5)),
-                                  case_label_aes)
-  # The following part should never be changed by users.
-  case_label_aes <- utils::modifyList(case_label_aes,
-                        list(data = dat[label_md, ],
-                             mapping = ggplot2::aes(
-                                x = .data[["row_id"]],
-                                y = .data[["md"]],
-                                label = .data[["case_id"]])))
-  p <- p + do.call(ggrepel::geom_label_repel, case_label_aes)
-  p
-}
-
-#' @noRd
-
-gcd_gof_plot_tmp <- function(influence_out,
-                         fit_measure,
-                         cutoff_gcd = NULL,
-                         cutoff_fit_measure = NULL,
-                         largest_gcd = 1,
-                         largest_fit_measure = 1,
-                         point_aes = list(),
-                         hline_aes = list(),
-                         cutoff_line_gcd_aes = list(),
-                         cutoff_line_fit_measures_aes = list(),
-                         case_label_aes = list()
-                         ) {
-  if (missing(influence_out)) {
-      stop("No influence_stat output supplied.")
-    }
-  if (missing(fit_measure)) {
-      stop("No fit_measure is selected.")
-    }
-  case_ids <- rownames(influence_out)
-  row_id   <- seq_len(nrow(influence_out))
-  dat <- data.frame(row_id = row_id,
-                    case_id = case_ids,
-                    influence_out,
-                    stringsAsFactors = FALSE,
-                    check.names = FALSE)
-  dat$fm <- dat[, fit_measure]
-
-  method <- attr(influence_out, "method")
-  if (method == "approx") {
-      dat$gcd <- dat$gcd_approx
-      gcd_label <- "Approximate Generalized Cook's Distance"
-      change_label <- "Approximate Change in Fit Measure"
-    } else {
-      gcd_label <- "Generalized Cook's Distance"
-      change_label <- "Change in Fit Measure"
-    }
-
-  point_aes <- utils::modifyList(list(),
-                                 point_aes)
-
-  hline_aes <- utils::modifyList(list(linetype = "solid"),
-                                  hline_aes)
-  # The following part should never be changed by users.
-  hline_aes <- utils::modifyList(hline_aes,
-                                  list(yintercept = 0))
-
-  p <- ggplot2::ggplot(dat, ggplot2::aes(.data$gcd, .data$fm))
-  p <- p + do.call(ggplot2::geom_point, point_aes)
-  p <- p + ggplot2::labs(title =
-            paste0(change_label, " against\n", gcd_label))
-  p <- p + do.call(ggplot2::geom_hline, hline_aes)
-  p <- p + ggplot2::xlab(gcd_label) +
-           ggplot2::ylab(change_label)
-
-  if (is.numeric(cutoff_fit_measure)) {
-      cutoff_line_fit_measures_aes <- utils::modifyList(list(linetype = "dashed"),
-                                    cutoff_line_fit_measures_aes)
-      # The following part should never be changed by users.
-      cutoff_line_fit_measures_aes1 <- utils::modifyList(cutoff_line_fit_measures_aes,
-                                    list(yintercept = cutoff_fit_measure))
-      cutoff_line_fit_measures_aes2 <- utils::modifyList(cutoff_line_fit_measures_aes,
-                                    list(yintercept = -cutoff_fit_measure))
-      p <- p + do.call(ggplot2::geom_hline, cutoff_line_fit_measures_aes1)
-      p <- p + do.call(ggplot2::geom_hline, cutoff_line_fit_measures_aes2)
-      c_fm_cut <- abs(cutoff_fit_measure)
-    } else {
-      c_fm_cut <- Inf
-    }
-  if (is.numeric(largest_fit_measure) && largest_fit_measure >= 1) {
-      m_fm <- round(largest_fit_measure)
-      o_fm <- order(abs(dat$fm), decreasing = TRUE)
-      m_fm_cut <- abs(dat$fm[o_fm[m_fm]])
-    } else {
-      m_fm_cut <- Inf
-    }
-  label_fm <- (abs(dat$fm) >= c_fm_cut) | (abs(dat$fm) >= m_fm_cut)
-
-  if (is.numeric(cutoff_gcd)) {
-      cutoff_line_gcd_aes <- utils::modifyList(list(linetype = "dashed"),
-                                    cutoff_line_gcd_aes)
-      # The following part should never be changed by users.
-      cutoff_line_gcd_aes <- utils::modifyList(cutoff_line_gcd_aes,
-                                    list(xintercept = cutoff_gcd))
-      p <- p + do.call(ggplot2::geom_vline, cutoff_line_gcd_aes)
-      c_gcd_cut <- cutoff_gcd
-    } else {
-      c_gcd_cut <- Inf
-    }
-  if (is.numeric(largest_gcd) && largest_gcd >= 1) {
-      m_gcd <- round(largest_gcd)
-      o_gcd <- order(dat$gcd, decreasing = TRUE)
-      m_gcd_cut <- dat$gcd[o_gcd[m_gcd]]
-    } else {
-      m_gcd_cut <- Inf
-    }
-  label_gcd <- (dat$gcd >= c_gcd_cut) | (dat$gcd >= m_gcd_cut)
-
-  case_label_aes <- utils::modifyList(list(),
-                                  case_label_aes)
-  # The following part should never be changed by users.
-  case_label_aes <- utils::modifyList(case_label_aes,
-                        list(data = dat[label_gcd | label_fm, ],
-                             mapping = ggplot2::aes(
-                                x = .data[["gcd"]],
-                                y = .data[["fm"]],
-                                label = .data[["case_id"]])))
-  p <- p + do.call(ggrepel::geom_label_repel, case_label_aes)
-
-  p
-}
-
-#' @noRd
-
-gcd_gof_md_plot_tmp <- function(influence_out,
-                            fit_measure,
-                            cutoff_md = FALSE,
-                            cutoff_fit_measure = NULL,
-                            circle_size = 2,
-                            cutoff_md_qchisq = .975,
-                            cutoff_gcd = NULL,
-                            largest_gcd = 1,
-                            largest_md = 1,
-                            largest_fit_measure = 1,
-                            point_aes = list(),
-                            hline_aes = list(),
-                            cutoff_line_md_aes = list(),
-                            cutoff_line_gcd_aes = list(),
-                            cutoff_line_fit_measures_aes = list(),
-                            case_label_aes = list()
-                            ) {
-  if (missing(influence_out)) {
-      stop("No influence_stat output supplied.")
-    }
-  if (missing(fit_measure)) {
-      stop("No fit_measure is selected.")
-    }
-  if (!inherits(attr(influence_out, "fit"), "lavaan")) {
-      stop(paste("The original lavaan output is not in the attributes.",
-                 "Was subsetting used to get influence_out?"))
-    }
-  fit0 <- attr(influence_out, "fit")
-  case_ids <- rownames(influence_out)
-  row_id   <- seq_len(nrow(influence_out))
-  dat <- data.frame(row_id = row_id,
-                    case_id = case_ids,
-                    influence_out,
-                    stringsAsFactors = FALSE,
-                    check.names = FALSE)
-  if (all(is.na(dat$md))) {
-      stop("All cases have no value on Mahalanobis distance (md).")
-    }
-  dat$fm <- dat[, fit_measure]
-
-  method <- attr(influence_out, "method")
-  if (method == "approx") {
-      dat$gcd <- dat$gcd_approx
-      gcd_label <- "Approximate Generalized Cook's Distance"
-      gcd_label_short <- "Approx. gCD"
-      change_label <- "Approximate Change in Fit Measure"
-    } else {
-      gcd_label <- "Generalized Cook's Distance"
-      gcd_label_short <- "gCD"
-      change_label <- "Change in Fit Measure"
-    }
-
-  point_aes <- utils::modifyList(list(shape = 21,
-                                      alpha = .50,
-                                      fill = "white"),
-                                 point_aes)
-  # The following part should never be changed by users.
-  point_aes <- utils::modifyList(point_aes,
-                        list(mapping = ggplot2::aes(size = .data[["gcd"]])))
-
-  hline_aes <- utils::modifyList(list(linetype = "solid"),
-                                  hline_aes)
-  # The following part should never be changed by users.
-  hline_aes <- utils::modifyList(hline_aes,
-                                  list(yintercept = 0))
-
-  p <- ggplot2::ggplot(dat, ggplot2::aes(.data$md, .data$fm))
-  p <- p + do.call(ggplot2::geom_point, point_aes)
-  p <- p + do.call(ggplot2::geom_hline, hline_aes)
-  p <- p + ggplot2::scale_size_area(name = gcd_label_short,
-                                  max_size = circle_size) +
-         ggplot2::labs(title =
-            paste0(change_label, " against Mahalanobis Distance,\n",
-                   gcd_label, " as the Size")) +
-         ggplot2::xlab("Mahalanobis Distance") +
-         ggplot2::ylab(change_label)
-
-  if (is.numeric(cutoff_fit_measure)) {
-      cutoff_line_fit_measures_aes <- utils::modifyList(list(linetype = "dashed"),
-                                    cutoff_line_fit_measures_aes)
-      # The following part should never be changed by users.
-      cutoff_line_fit_measures_aes1 <- utils::modifyList(cutoff_line_fit_measures_aes,
-                                    list(yintercept = cutoff_fit_measure))
-      cutoff_line_fit_measures_aes2 <- utils::modifyList(cutoff_line_fit_measures_aes,
-                                    list(yintercept = -cutoff_fit_measure))
-      p <- p + do.call(ggplot2::geom_hline, cutoff_line_fit_measures_aes1)
-      p <- p + do.call(ggplot2::geom_hline, cutoff_line_fit_measures_aes2)
-      c_fm_cut <- abs(cutoff_fit_measure)
-    } else {
-      c_fm_cut <- Inf
-    }
-  if (is.numeric(largest_fit_measure) && largest_fit_measure >= 1) {
-      m_fm <- round(largest_fit_measure)
-      o_fm <- order(abs(dat$fm), decreasing = TRUE)
-      m_fm_cut <- abs(dat$fm[o_fm[m_fm]])
-    } else {
-      m_fm_cut <- Inf
-    }
-  label_fm <- (abs(dat$fm) >= c_fm_cut) | (abs(dat$fm) >= m_fm_cut)
-
-  k <- ncol(fit0@Data@X[[1]])
-  c_md_cut <- Inf
-  if (isTRUE(cutoff_md)) {
-      c_md_cut <- stats::qchisq(cutoff_md_qchisq, k)
-    }
-  if (is.numeric(cutoff_md)) {
-      c_md_cut <- cutoff_md
-    }
-  if (is.numeric(largest_md) && largest_md >= 1) {
-      m_md <- round(largest_md)
-      o_md <- order(dat$md, decreasing = TRUE)
-      m_md_cut <- dat$md[o_md[m_md]]
-    } else {
-      m_md_cut <- Inf
-    }
-  if (is.numeric(c_md_cut) && c_md_cut < Inf) {
-      cutoff_line_md_aes <- utils::modifyList(list(linetype = "dashed"),
-                                    cutoff_line_md_aes)
-      # The following part should never be changed by users.
-      cutoff_line_md_aes <- utils::modifyList(cutoff_line_md_aes,
-                                    list(xintercept = c_md_cut))
-      p <- p + do.call(ggplot2::geom_vline, cutoff_line_md_aes)
-    }
-  label_md <- (dat$md >= c_md_cut) | (dat$md >= m_md_cut)
-
-  if (is.numeric(cutoff_gcd)) {
-      c_gcd_cut <- cutoff_gcd
-    } else {
-      c_gcd_cut <- Inf
-    }
-  if (is.numeric(largest_gcd) && largest_gcd >= 1) {
-      m_gcd <- round(largest_gcd)
-      o_gcd <- order(dat$gcd, decreasing = TRUE)
-      m_gcd_cut <- dat$gcd[o_gcd[m_gcd]]
-    } else {
-      m_gcd_cut <- Inf
-    }
-  label_gcd <- (dat$gcd >= c_gcd_cut) | (dat$gcd >= m_gcd_cut)
-
-  case_label_aes <- utils::modifyList(list(),
-                                  case_label_aes)
-  # The following part should never be changed by users.
-  case_label_aes <- utils::modifyList(case_label_aes,
-                        list(data = dat[label_fm | label_md | label_gcd, ],
-                             mapping = ggplot2::aes(
-                                x = .data[["md"]],
-                                y = .data[["fm"]],
                                 label = .data[["case_id"]])))
   p <- p + do.call(ggrepel::geom_label_repel, case_label_aes)
   p
--- tests/testthat/test-index_plot.R
+++ tests/testthat/test-index_plot.R
@@ -1,12 +1,5 @@
-skip("WIP")
 library(testthat)
 library(lavaan)
-library(semfindr)
-
-# A path model
-# fixed.x: TRUE (default)
-# Labelled: Some are labelled
-# User-defined parameters: At least one
 
 mod <-
 '
@@ -23,32 +16,59 @@ fit <- lavaan::sem(mod, dat0)
 rerun_out <- lavaan_rerun(fit, parallel = FALSE,
                           to_rerun = 1:10)
 fit_est_change <- est_change(rerun_out)
-fit_est_change_approx <- est_change_approx(fit)
 fit_est_change_raw <- est_change_raw(rerun_out)
-fit_est_change_raw_approx <- est_change_raw_approx(fit)
-fit_est_change_raw_std <- est_change_raw(rerun_out,
-                                         standardized = TRUE)
 inf_out <- influence_stat(rerun_out)
-inf_approx_out <- influence_stat(fit)
 
-index_plot(fit_est_change, "gcd")
-index_plot(fit_est_change_raw, "m1~iv2")
-index_plot(fit_est_change_raw, "m1~iv2",
-           largest_x = 5)
+p <- index_plot(fit_est_change, "gcd")
+test_that("index_plot", {
+    expect_equal(p$data$x,
+                 fit_est_change[, "gcd"],
+                 ignore_attr = TRUE)
+    expect_equal(p$layers[[4]]$data[1, "row_id"], 9)
+  })
 
-index_plot(inf_out, "chisq",
-           x_label = "Chi-Square Influence",
-           largest_x = 3,
-           cutoff_x_high = .08,
-           cutoff_x_low = -.25)
-index_plot(inf_out, "chisq",
-           x_label = "Chi-Square Influence",
-           absolute = TRUE,
-           largest_x = 3,
-           cutoff_x_high = .08)
-index_plot(inf_out, "gcd",
-           x_label = "gCD")
-index_plot(inf_out, "gcd",
-           x_label = "gCD",
-           largest_x = 3,
-           cutoff_x = .05)
+p <- index_plot(fit_est_change_raw, "m1~iv2")
+test_that("index_plot", {
+    expect_equal(p$data$x,
+                 fit_est_change_raw[, "m1~iv2"],
+                 ignore_attr = TRUE)
+    expect_equal(p$layers[[4]]$data[1, "row_id"], 8)
+  })
+
+p <- index_plot(fit_est_change_raw, "m1~iv2",
+                largest_x = 5)
+test_that("index_plot", {
+    expect_equal(p$data$x,
+                 fit_est_change_raw[, "m1~iv2"],
+                 ignore_attr = TRUE)
+    expect_equal(nrow(p$layers[[4]]$data), 5)
+    expect_equal(p$labels$y, "Statistic")
+  })
+
+p <- index_plot(inf_out, "chisq",
+                x_label = "Chi-Square Influence",
+                largest_x = 3,
+                cutoff_x_high = .08,
+                cutoff_x_low = -.25)
+test_that("index_plot", {
+    expect_equal(p$data$x,
+                 inf_out[, "chisq"],
+                 ignore_attr = TRUE)
+    expect_equal(p$layers[[4]]$data[1, 1], -.25)
+    expect_equal(p$layers[[5]]$data[1, 1], .08)
+    expect_equal(nrow(p$layers[[6]]$data), 6)
+    expect_equal(p$labels$y, "Chi-Square Influence")
+  })
+
+p <- index_plot(inf_out, "chisq",
+                absolute = TRUE,
+                largest_x = 3,
+                cutoff_x_high = .08)
+test_that("index_plot", {
+    expect_equal(p$data$x,
+                 abs(inf_out[, "chisq"]),
+                 ignore_attr = TRUE)
+    expect_equal(p$layers[[4]]$data[1, 1], .08)
+    expect_equal(nrow(p$layers[[5]]$data), 6)
+    expect_equal(p$labels$y, "Absolute(Statistic)")
+  })
