712b471995c74a48aad1e3dd35dbc400c27b30a2
Shu Fai Cheung <shufai.cheung@gmail.com>
2025-03-06 19:36:40 +0800

    Update rebuild_vignettes.R

--- rebuild_vignettes.R
+++ rebuild_vignettes.R
@@ -3,7 +3,10 @@
 base_dir <- getwd()
 
 setwd("vignettes/")
+
 knitr::knit("semfindr.Rmd.original", output = "semfindr.Rmd")
+knitr::knit("casewise_scores.Rmd.original", output = "casewise_scores.Rmd")
+knitr::knit("user_id.Rmd.original", output = "user_id.Rmd")
 
 setwd(base_dir)
 
@@ -12,6 +15,7 @@ setwd(base_dir)
 base_dir <- getwd()
 
 setwd("vignettes/articles")
+
 knitr::knit("user_function.Rmd.original", output = "user_function.Rmd")
 
 setwd(base_dir)
